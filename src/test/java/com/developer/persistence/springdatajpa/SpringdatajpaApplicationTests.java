8d505eaed1f692c00b447b382d845e935443b347
leonelmfonseca <leonelfonseca@gmail.com>
2025-03-05 17:23:25 +0000

    style: add todo comment

--- src/test/java/com/developer/persistence/springdatajpa/SpringdatajpaApplicationTests.java
+++ src/test/java/com/developer/persistence/springdatajpa/SpringdatajpaApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringdatajpaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {
+    // TODO document why this method is empty
+  }
 }
